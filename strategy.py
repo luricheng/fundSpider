8231543ab5926576fdc2299fb83b3883eaaa2b8f
luricheng <luricheng@bytedance.com>
2019-10-01 03:06:18 +0800

    analy

---
+++ strategy.py
@@ -0,0 +1,50 @@
+
+class Strategy(object):
+    @classmethod
+    def bottom_suction_high_throw(cls,
+                                  value_list,
+                                  start_idx=0,
+                                  end_idx=None,
+                                  base_money=100,
+                                  add_money=10,
+                                  take_profit_rate=0.05):
+        """
+        低吸高抛策略, 一只基金从发行到现在 投入资金&回报
+        :param value_list: 基金净值list
+        :param start_idx: 开始投资时刻
+        :param end_idx: 结束投资时刻
+        :param base_money: 建仓成本
+        :param add_money: 每次加仓成本
+        :param take_profit_rate: 止盈率
+        :return:
+        """
+        if len(value_list) < 2:
+            raise Exception("value_list's length must > 1, but got %s" % len(value_list))
+        end_idx = end_idx or len(value_list)
+        end_idx = min(end_idx, len(value_list))
+        total_cost = base_money                             # 总成本
+        holding_share = 1.0 * base_money / value_list[start_idx]    # 持有份额
+        for i in range(start_idx+1, end_idx):
+            value = value_list[i]
+            pre_value = value_list[i-1]
+            # 跌
+            if value < pre_value:
+                total_cost += add_money
+                holding_share += 1.0 * add_money / value
+            # 涨 收益 > 止盈率
+            if i + 1 == end_idx or cls._get_growth_rate(total_cost, holding_share, value) > take_profit_rate:
+                return {
+                    "cost_time": i-start_idx,
+                    "total_cost": total_cost,
+                    "profit_rate": cls._get_growth_rate(total_cost, holding_share, value),
+                }
+
+    @staticmethod
+    def _get_growth_rate(cost, holding_share, value):
+        cur_val = 1.0 * holding_share * value
+        return (cur_val - cost) / cost
+
+
+if __name__ == '__main__':
+    value_list = [1.0, 1.01, 1.0125, 0.99, 0.97, 1.009, 1.029, 1.059]
+    print(Strategy.bottom_suction_high_throw(value_list))
---
+++ tool.py
@@ -0,0 +1,35 @@
+import pickle
+import numpy as np
+import os
+
+
+class Tool(object):
+    FUND_INFO_DICT_PKL = "pkl_data/fund_info_dict.pkl"
+    HIGH_RISK_TYPES = ["股票指数", "混合型", "股票型"]
+
+    @classmethod
+    def load_fund_info_by_type(cls, fund_types=None, base_path='.'):
+        fund_types = fund_types or cls.HIGH_RISK_TYPES
+        path = os.path.join(base_path, cls.FUND_INFO_DICT_PKL)
+        with open(path, 'rb') as fp:
+            fund_info_dict = pickle.load(fp)
+        target_funds = {}
+        for k, v in fund_info_dict.items():
+            if v['type'] in fund_types:
+                target_funds[k] = v
+        return target_funds
+
+    @staticmethod
+    def get_value_list(fund, unit_value=False):
+        detail = fund.get("detail")
+        date_value = []
+        value_name = "unit_value" if unit_value else "cumulative_value"
+        for k, v in detail.items():
+            date_value.append((k, float(v.get(value_name))))
+        date_value.sort(key=lambda x: x[0])
+        return [x[1] for x in date_value]
+
+
+if __name__ == '__main__':
+    f = Tool.load_fund_info_by_type()
+    print(Tool.get_value_list(f[161725]))
